fc2e67d766962174dbbdc8580d93838c2b5fa060
DAVIDK <dk6216720@gmail.com>
2023-03-25 15:24:59 -0400

    Add running score and announced winner of game at end of 5 rounds

--- javascript-file.js
+++ javascript-file.js
@@ -1,3 +1,32 @@
+// Count wins
+
+    let playerWinTally = 0;
+    let computerWinTally = 0;
+    let roundCounter = 0;
+
+    
+
+// Create Three Buttons, one for each rps selection
+    const playerChoiceRock = document.querySelector('#Rock');
+    playerChoiceRock.addEventListener('click', () => {
+        playRound('Rock', getComputerChoice());
+    });
+
+    const playerChoicePaper = document.querySelector('#Paper');
+    playerChoicePaper.addEventListener('click', () => {
+        playRound('Paper', getComputerChoice());
+    });
+
+    const playerChoiceScissors = document.querySelector('#Scissors');
+    playerChoiceScissors.addEventListener('click', () => {
+        playRound('Scissors', getComputerChoice());
+    });
+
+// Add a div for displaying results and change all of your console.log into DOM methods
+    const results = document.querySelector('.results');
+
+    
+
 /** Get Player Choice */
     function getPlayerChoice () {
         let userChoice = prompt("Rock, Paper, Scissors?");
@@ -38,55 +67,158 @@
     function playRound(playerSelection, computerSelection){
         if (playerSelection === 'Rock'){
             if (computerSelection === 'Paper'){
-                alert('You Lose! Paper beats Rock!');
-                return 'You Lose!';
+                computerWinTally += 1;
+                roundCounter += 1;
+                if (roundCounter === 5 && playerWinTally > computerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Player Wins!`;              
+                }
+                if (roundCounter === 5 && computerWinTally > playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer Wins!`;
+                }
+                if (roundCounter === 5 && computerWinTally === playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer and Player are Tied! DRAW!`;
+                }
+                if (roundCounter < 5){
+                    results.textContent = `Round ${roundCounter}: You Lose! Paper beats Rock! Player Win Total: ${playerWinTally}; Computer Win Total: ${computerWinTally}`;
+                }
             } else if (computerSelection === 'Scissors'){
-                alert('You Win! Rock beats Scissors!');
-                return 'You Win!';
+                playerWinTally += 1;
+                roundCounter += 1;
+                if (roundCounter === 5 && playerWinTally > computerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Player Wins!`; 
+                }
+                if (roundCounter === 5 && computerWinTally > playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer Wins!`;
+                }
+                if (roundCounter === 5 && computerWinTally === playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer and Player are Tied! DRAW!`;
+                }
+                if (roundCounter < 5){
+                    results.textContent = `Round ${roundCounter}: You Win! Rock beats Scissors! Player Win Total: ${playerWinTally}; Computer Win Total: ${computerWinTally}`;
+                }
             } else {
-                alert('You Tied! Rock and rock, the less popular cousin to rock and roll!');
-                return 'You Tied!';
+                roundCounter += 1;
+                if (roundCounter === 5 && playerWinTally > computerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Player Wins!`;
+                    
+                }
+                if (roundCounter === 5 && computerWinTally > playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer Wins!`;
+                }
+                if (roundCounter === 5 && computerWinTally === playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer and Player are Tied! DRAW!`;
+                }
+                if (roundCounter < 5){
+                    results.textContent = `Round ${roundCounter}: You Tied! Player Win Total: ${playerWinTally}; Computer Win Total: ${computerWinTally}`;
+                }
             }
         }
         if (playerSelection === 'Paper'){
             if (computerSelection === 'Rock'){
-                alert('You Win! Paper beats Rock!');
-                return 'You Win!';
+                playerWinTally += 1;
+                roundCounter += 1;
+                if (roundCounter === 5 && playerWinTally > computerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Player Wins!`;              
+                }
+                if (roundCounter === 5 && computerWinTally > playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer Wins!`;
+                }
+                if (roundCounter === 5 && computerWinTally === playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer and Player are Tied! DRAW!`;
+                }
+                if (roundCounter < 5){
+                    results.textContent = `Round ${roundCounter}: You Lose! Paper beats Rock! Player Win Total: ${playerWinTally}; Computer Win Total: ${computerWinTally}`;
+                }
+                
+                
             } else if (computerSelection === 'Scissors'){
-                alert('You Lose! Scissors beats Paper!');
-                return 'You Lose!';
+                computerWinTally += 1;
+                roundCounter += 1;
+                if (roundCounter === 5 && playerWinTally > computerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Player Wins!`;              
+                }
+                if (roundCounter === 5 && computerWinTally > playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer Wins!`;
+                }
+                if (roundCounter === 5 && computerWinTally === playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer and Player are Tied! DRAW!`;
+                }
+                if (roundCounter < 5){
+                    results.textContent = `Round ${roundCounter}: You Lose! Paper beats Rock! Player Win Total: ${playerWinTally}; Computer Win Total: ${computerWinTally}`;
+                }
+
             } else {
-                alert('You Tied! Paper and paper make for a wet towel fight!');
-                return 'You Tied!';
+                roundCounter += 1;
+                if (roundCounter === 5 && playerWinTally > computerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Player Wins!`;              
+                }
+                if (roundCounter === 5 && computerWinTally > playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer Wins!`;
+                }
+                if (roundCounter === 5 && computerWinTally === playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer and Player are Tied! DRAW!`;
+                }
+                if (roundCounter < 5){
+                    results.textContent = `Round ${roundCounter}: You Lose! Paper beats Rock! Player Win Total: ${playerWinTally}; Computer Win Total: ${computerWinTally}`;
+                }
+                
+                
             }
         }
         if (playerSelection === 'Scissors'){
             if (computerSelection === 'Rock'){
-                alert('You Lose! Rock beats Scissors!');
-                return 'You Lose!';
+                computerWinTally += 1;
+                roundCounter += 1;
+                if (roundCounter === 5 && playerWinTally > computerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Player Wins!`;              
+                }
+                if (roundCounter === 5 && computerWinTally > playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer Wins!`;
+                }
+                if (roundCounter === 5 && computerWinTally === playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer and Player are Tied! DRAW!`;
+                }
+                if (roundCounter < 5){
+                    results.textContent = `Round ${roundCounter}: You Lose! Paper beats Rock! Player Win Total: ${playerWinTally}; Computer Win Total: ${computerWinTally}`;
+                }
+                
+                
             } else if (computerSelection === 'Paper'){
-                alert('You Win! Scissors beats Paper!');
-                return 'You Win!';
+                playerWinTally += 1;
+                roundCounter += 1;
+                if (roundCounter === 5 && playerWinTally > computerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Player Wins!`;              
+                }
+                if (roundCounter === 5 && computerWinTally > playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer Wins!`;
+                }
+                if (roundCounter === 5 && computerWinTally === playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer and Player are Tied! DRAW!`;
+                }
+                if (roundCounter < 5){
+                    results.textContent = `Round ${roundCounter}: You Lose! Paper beats Rock! Player Win Total: ${playerWinTally}; Computer Win Total: ${computerWinTally}`;
+                }
+               
+                
             } else {
-                alert('You Tied! Scissors and scissors is dangerous though, no running around!');
-                return 'You Tied!';
+                roundCounter += 1;
+                if (roundCounter === 5 && playerWinTally > computerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Player Wins!`;              
+                }
+                if (roundCounter === 5 && computerWinTally > playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer Wins!`;
+                }
+                if (roundCounter === 5 && computerWinTally === playerWinTally){
+                    results.textContent = `5 ROUND MATCH RESULTS: Computer and Player are Tied! DRAW!`;
+                }
+                if (roundCounter < 5){
+                    results.textContent = `Round ${roundCounter}: You Lose! Paper beats Rock! Player Win Total: ${playerWinTally}; Computer Win Total: ${computerWinTally}`;
+                }
+                
+                
             }
             }
         };
-// Create Three Buttons, one for each rps selection
-    const playerChoiceRock = document.querySelector('#Rock');
-    playerChoiceRock.addEventListener('click', () => {
-        playRound('Rock', getComputerChoice);
-    });
 
-    const playerChoicePaper = document.querySelector('#Paper');
-    playerChoicePaper.addEventListener('click', () => {
-        playRound('Paper', getComputerChoice);
-    });
 
-    const playerChoiceScissors = document.querySelector('#Scissors');
-    playerChoiceScissors.addEventListener('click', () => {
-        playRound('Scissors', getComputerChoice);
-    });
 
-// Add a div for displaying results and change all of your console.log into DOM methods
